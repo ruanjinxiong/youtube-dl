041a49840c5d416a626843aac8323de7b6fb1229
ruanjinxiong <nguyentienhung@nguyens-MBP.net.fpt>
2018-09-27 02:29:49 +0700

    zingmp3 extractor

--- youtube_dl/extractor/zingmp3.py
+++ youtube_dl/extractor/zingmp3.py
@@ -1,92 +1,9 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
-import re
-
 from .common import InfoExtractor
-from ..utils import (
-    ExtractorError,
-    int_or_none,
-    update_url_query,
-)
 
-
-class ZingMp3BaseInfoExtractor(InfoExtractor):
-
-    def _extract_item(self, item, page_type, fatal=True):
-        error_message = item.get('msg')
-        if error_message:
-            if not fatal:
-                return
-            raise ExtractorError(
-                '%s returned error: %s' % (self.IE_NAME, error_message),
-                expected=True)
-
-        formats = []
-        for quality, source_url in zip(item.get('qualities') or item.get('quality', []), item.get('source_list') or item.get('source', [])):
-            if not source_url or source_url == 'require vip':
-                continue
-            if not re.match(r'https?://', source_url):
-                source_url = '//' + source_url
-            source_url = self._proto_relative_url(source_url, 'http:')
-            quality_num = int_or_none(quality)
-            f = {
-                'format_id': quality,
-                'url': source_url,
-            }
-            if page_type == 'video':
-                f.update({
-                    'height': quality_num,
-                    'ext': 'mp4',
-                })
-            else:
-                f.update({
-                    'abr': quality_num,
-                    'ext': 'mp3',
-                })
-            formats.append(f)
-
-        cover = item.get('cover')
-
-        return {
-            'title': (item.get('name') or item.get('title')).strip(),
-            'formats': formats,
-            'thumbnail': 'http:/' + cover if cover else None,
-            'artist': item.get('artist'),
-        }
-
-    def _extract_player_json(self, player_json_url, id, page_type, playlist_title=None):
-        player_json = self._download_json(player_json_url, id, 'Downloading Player JSON')
-        items = player_json['data']
-        if 'item' in items:
-            items = items['item']
-
-        if len(items) == 1:
-            # one single song
-            data = self._extract_item(items[0], page_type)
-            data['id'] = id
-
-            return data
-        else:
-            # playlist of songs
-            entries = []
-
-            for i, item in enumerate(items, 1):
-                entry = self._extract_item(item, page_type, fatal=False)
-                if not entry:
-                    continue
-                entry['id'] = '%s-%d' % (id, i)
-                entries.append(entry)
-
-            return {
-                '_type': 'playlist',
-                'id': id,
-                'title': playlist_title,
-                'entries': entries,
-            }
-
-
-class ZingMp3IE(ZingMp3BaseInfoExtractor):
+class ZingMp3IE(InfoExtractor):
     _VALID_URL = r'https?://mp3\.zing\.vn/(?:bai-hat|album|playlist|video-clip)/[^/]+/(?P<id>\w+)\.html'
     _TESTS = [{
         'url': 'http://mp3.zing.vn/bai-hat/Xa-Mai-Xa-Bao-Thy/ZWZB9WAB.html',
@@ -131,13 +48,26 @@ class ZingMp3IE(ZingMp3BaseInfoExtractor):
             r'&amp;xmlURL=([^&]+)&'
         ], webpage, 'player xml url')
 
-        playlist_title = None
-        page_type = self._search_regex(r'/(?:html5)?xml/([^/-]+)', player_json_url, 'page type')
-        if page_type == 'video':
-            player_json_url = update_url_query(player_json_url, {'format': 'json'})
-        else:
-            player_json_url = player_json_url.replace('/xml/', '/html5xml/')
-            if page_type == 'album':
-                playlist_title = self._og_search_title(webpage)
+        player_json_url = "https://mp3.zing.vn/xhr" + player_json_url
 
-        return self._extract_player_json(player_json_url, page_id, page_type, playlist_title)
+        playlist_title = None
+        page_type = self._search_regex(r'type=([^/-]+)&', player_json_url, 'page type')
+
+        player_json = self._download_json(player_json_url, page_id, 'Downloading Player JSON')
+        items = player_json['data']
+        source = items.get('source')
+        print(source.get('128'))
+        return {
+            'id': page_id,
+            'title': (items.get('name') or item.get('title')).strip(),
+            'url': source.get('128'),
+            'ext': 'mp3',
+            'thumbnail': items.get('thumbnail'),
+            'artist': items.get('artist'),
+        }
+
+
+
+
+
+
